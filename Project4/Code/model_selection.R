a197ce06a401be861f9b9ef3fa90b8c9287399c3
Joseph Nagel <joseph.nagel@cuanschutz.edu>
2026-04-30 19:17:10 -0600

    added parallel processing

--- Project4/Code/model_selection.R
+++ Project4/Code/model_selection.R
@@ -1,19 +1,23 @@
 .create_formula <- function(lhs = "y", rhs = NULL) {
     if (is.null(rhs) || length(rhs) == 0) {
-        formula_str <- paste(lhs, "~ 1")
+        formula_str <- paste(lhs, "~ 0")
     } else {
-        formula_str <- paste(lhs, "~", paste(rhs, collapse = " + "))
+        formula_str <- paste(lhs, "~ 0 + ", paste(rhs, collapse = " + "))
     }
     return(as.formula(formula_str))
 }
 
 .get_nonzero_coef_names <- function(coef) {
-    coef_names <- rownames(coef)[as.numeric(coef) != 0]
-    return(coef_names[coef_names != "(Intercept)"])
+    return(rownames(coef)[as.numeric(coef) != 0])
 }
 
 .get_reduced_models_cv_glmnet <- function(simulated_data, alpha) {
-    cv_fit <- glmnet::cv.glmnet(x = simulated_data$X, y = simulated_data$y, alpha = alpha)
+    cv_fit <- glmnet::cv.glmnet(
+        x = simulated_data$X,
+        y = simulated_data$y,
+        alpha = alpha,
+        intercept = FALSE
+    )
     
     coef_lambda_min <- glmnet::coef.glmnet(cv_fit, s = "lambda.min")
     coef_lambda_1se <- glmnet::coef.glmnet(cv_fit, s = "lambda.1se")
@@ -28,18 +32,42 @@
     return(list(reduced_model_min = reduced_model_min, reduced_model_1se = reduced_model_1se))
 }
 
+# .get_reduced_model_p <- function(full_model, simulated_data, p_val) {
+#     reduced_model_p_ols <- olsrr::ols_step_backward_p(full_model, p_val = p_val)
+#     
+#     reduced_formula <- formula(reduced_model_p_ols$model)
+#     df <- data.frame(y = simulated_data$y, simulated_data$X)
+#     
+#     return(lm(formula = reduced_formula, data = df))
+# }
+
 .get_reduced_model_p <- function(full_model, simulated_data, p_val) {
-    reduced_model_p_ols <- olsrr::ols_step_backward_p(full_model, p_val = p_val)
+    current_model <- full_model
     
-    reduced_formula <- formula(reduced_model_p_ols$model)
-    df <- data.frame(y = simulated_data$y, simulated_data$X)
+    repeat {
+        current_terms <- attr(terms(current_model), "term.labels")
+        if (length(current_terms) == 0) break
+        
+        test_results <- drop1(current_model, test = "F")
+        if (!"Pr(>F)" %in% colnames(test_results)) break
+        
+        p_values <- test_results[["Pr(>F)"]][-1]
+        if (is.null(p_values) || length(p_values) == 0) break
+        names(p_values) <- current_terms
+        
+        max_p <- max(p_values, na.rm = TRUE)
+        if (is.na(max_p) || max_p <= p_val) break
+        
+        var_to_drop <- names(p_values)[which.max(p_values)]
+        current_model <- update(current_model, as.formula(paste(". ~ . -", var_to_drop)))
+    }
     
-    return(lm(formula = reduced_formula, data = df))
+    return(current_model)
 }
 
 get_reduced_models <- function(simulated_data, alpha, p_val) {
     n_obs <- length(simulated_data$y)
-    full_model <- lm(y ~ ., data = data.frame(y = simulated_data$y, simulated_data$X))
+    full_model <- lm(y ~ 0 + ., data = data.frame(y = simulated_data$y, simulated_data$X))
     
     lasso = .get_reduced_models_cv_glmnet(simulated_data, alpha = 1)
     elanet = .get_reduced_models_cv_glmnet(simulated_data, alpha = alpha)
